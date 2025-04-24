bfe1c665149a7bf48a0fb63c4c25ff41a49025bf
saad nasim <saad.nasim42@gmail.com>
2025-04-25 03:26:41 +0500

    dynamic env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,30 @@
 <?php
 
+use Dotenv\Dotenv;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\App;
 
+$host = $_SERVER['DB_HOST'] ?? '.env.dev'; 
+switch ($host) {
+    case 'dev':
+        $envFile = '.env.dev';
+        break;
+    case 'staging':
+        $envFile = '.env.stage';
+        break;
+    case 'production':
+        $envFile = '.env.production';
+        break;
+    default:
+        $envFile = '.env.dev';
+        break;
+}
+
+if (file_exists(__DIR__.'/../'.$envFile)) {
+    $dotenv = Dotenv::createImmutable(__DIR__.'/../', $envFile);
+    $dotenv->load();
+}
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
