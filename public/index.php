d4d5a0e934470503edfa17e7b9216c820bea7255
saad nasim <saad.nasim42@gmail.com>
2025-04-25 01:37:13 +0500

    project initalise

--- public/index.php
+++ public/index.php
@@ -13,8 +13,29 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+
+$host = $_SERVER['DB_HOST'];
+switch ($host) {
+    case 'dev':
+        $envFile = '.env.dev';
+        break;
+    case 'staging':
+        $envFile = '.env.stage';
+        break;
+    case 'production':
+        $envFile = '.env.production';
+        break;
+    default:
+        $envFile = '.env.dev';
+        break;
+}
+
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../', $envFile);
+$dotenv->load();
 
 $app->handleRequest(Request::capture());
