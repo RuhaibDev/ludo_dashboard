994ed2beadb25eca26ea83fe97dfda1296664c6e
saad nasim <saad.nasim42@gmail.com>
2025-04-26 19:51:03 +0500

    used services

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Resources\UserResource;
-use App\Repositories\UserRepository\UserRepository;
+use App\Services\UserService;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserController extends Controller
 {
-    public function __construct(public UserRepository $userRepository){}
+    public function __construct(public UserService $userService){}
 
     /**
      * @throws \Throwable
@@ -18,7 +18,7 @@ class UserController extends Controller
     public function create(StoreUserRequest $request): JsonResource
     {
         $filteredResponse = $request->validated();
-        $response = $this->userRepository->createUser($filteredResponse);
+        $response = $this->userService->createUser($filteredResponse);
         return new UserResource($response);
     }
 }
