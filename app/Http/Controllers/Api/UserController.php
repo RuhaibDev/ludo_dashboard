f0bcf32520cb355873238be897f582cae8701df9
saad nasim <saad.nasim42@gmail.com>
2025-04-26 07:18:05 +0500

    api response modified

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
 use App\Repositories\UserRepository\UserRepository;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserController extends Controller
 {
-    public function __construct(public UserRepository $userRepository) {}
-    public function create(StoreUserRequest $request): JsonResponse
+    public function __construct(public UserRepository $userRepository){}
+
+    /**
+     * @throws \Throwable
+     */
+    public function create(StoreUserRequest $request): JsonResource
     {
         $filteredResponse = $request->validated();
         $response = $this->userRepository->createUser($filteredResponse);
-        return success($response);
+        return new UserResource($response);
     }
 }
